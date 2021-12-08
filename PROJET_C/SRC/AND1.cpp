93134f2b646e2fd563781b4e5146860d2ba5493f
deranore <74006932+deranore@users.noreply.github.com>
2021-12-08 09:33:57 +0100

    Porte Complié avant main de test unitaire

--- PROJET_C/SRC/AND1.cpp
+++ PROJET_C/SRC/AND1.cpp
@@ -7,28 +7,28 @@
 using namespace std;
 
 //Constructeur
-AND1(string _nom,vector<string> _input,string _output,unsigned int _delay=0):Comb(_nom,_input,_output,_delay){
+AND1::AND1(string _nom,vector<string> _input,string _output,unsigned int _delay):Comb(_nom,_input,_output,_delay){
   cout << " Constructeur de porte AND X vers 1" << endl;
 }
 
-~AND1(){
+AND1::~AND1(){
   cout << " Destrcteur de porte AND X vers 1 " << endl;
 }
 
 //Fonction virtuelle pure
-bool calcul_output(input map(string,net)){
+bool AND1::calcul_output(map<string,Net> input){
   bool output = 1;
-  for (vector<string>::iterator index = this->input.begin(); index != this->input.end();index++){
-    output &= input[*index];
+  for (vector<string>::iterator index = get_input().begin(); index != get_input().end();index++){
+    output &= input[*index].get_val();
   }
   return output;
 }
 
-void afficher(){
+void AND1::afficher(){
   cout << "Addresse : " << this << endl;
-  cout << "Nom : " << this->get_nom() << endl;
-  cout << "Porte AND 1 sortie pour un nombre de sortie de : " << this->input.size() << endl;
+  cout << "Nom : " << this->get_name() << endl;
+  //cout << "Porte AND 1 sortie pour un nombre de sortie de : " << this->input.size() << endl;
   cout << "Net de sortie : " << this->get_output() << endl;
-  cout << "Net d'entrée : " << this->get_input() << endl;
+  //cout << "Net d'entrée : " << this->get_input() << endl;
   cout << "Delay : " << this->get_delay() << endl;
 }
